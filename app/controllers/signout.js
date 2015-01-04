3c96e33ccd297c6132db5b56190becbbe7145887
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-01-04 18:06:47 +0200

    Callback added to signout.

--- app/controllers/signout.js
+++ app/controllers/signout.js
@@ -2,5 +2,10 @@
 
 exports.get = function (req, res, next) {
 	req.logout();
+	if (req.session.callbackUrl) {
+		var callbackUrl = req.session.callbackUrl;
+		req.session.callbackUrl = null;
+		return res.redirect(callbackUrl);
+	}
 	res.redirect('/signin');
 };
