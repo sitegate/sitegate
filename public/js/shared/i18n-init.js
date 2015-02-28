272ee4e1b50c5af5127b61531e7500a559bdb635
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-02-28 14:28:27 +0200

    Front-end part of i18next fixed. humans.txt added.

--- public/js/shared/i18n-init.js
+++ public/js/shared/i18n-init.js
@@ -5,10 +5,12 @@
 
   $.i18n.init({
     lng: 'en',
+    fallbackLng: 'en',
     ns: {
       namespaces: ['translation'],
       defaultNs: 'translation'
-    }
+    },
+    resGetPath: '/locales/__lng__/__ns__.json'
   });
 
   window._t = function (resourceName) {
