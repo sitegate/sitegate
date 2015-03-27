95bb5258eacec567e96e7b24dcecfe2823bfcf81
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-03-27 23:52:47 +0200

    Dependencies updated.

--- config/strategies/bearer.js
+++ config/strategies/bearer.js
@@ -5,9 +5,5 @@ var BearerStrategy = require('passport-http-bearer').Strategy;
 var OAuth = require('../../app/clients/oauth');
 
 module.exports = function () {
-  passport.use(new BearerStrategy(
-    function (accessToken, done) {
-      OAuth.authToken(accessToken, done);
-    }
-  ));
+  passport.use(new BearerStrategy(OAuth.authToken));
 };
