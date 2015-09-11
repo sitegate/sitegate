1b9dd588c453bd29455511b160d66582c78607fa
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-09-12 00:47:16 +0300

    foso used for bundling.

--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,38 @@
 
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
-var livereload = require('gulp-livereload');
-var sass = require('gulp-sass');
+var foso = require('foso');
+var js = require('fosify-js');
+var sass = require('fosify-sass');
 var fs = require('fs');
-
-gulp.task('sass', function () {
-  gulp.src(__dirname + '/public/css/**/*.sass')
-    .pipe(sass({
-      errLogToConsole: true,
-      indentedSyntax: true
-    }))
-    .pipe(gulp.dest(__dirname + '/public/dest/css'))
-    .pipe(livereload());
-});
-
-gulp.task('watch', function () {
-  gulp.watch(__dirname + '/public/css/**/*.sass', ['sass']);
-});
 
 gulp.task('develop', function () {
   var options = {
-    key: fs.readFileSync(__dirname + '/certs/privatekey.pem'),
-    cert: fs.readFileSync(__dirname + '/certs/certificate.pem')
+    src: './public/src',
+    dest: './public/dist',
+    watch: true,
+    livereload: {
+      key: fs.readFileSync(__dirname + '/certs/privatekey.pem'),
+      cert: fs.readFileSync(__dirname + '/certs/certificate.pem')
+    }
   };
-  livereload.listen(options);
+
+  foso
+    .please(options)
+    .fosify(js)
+    .fosify(sass)
+    .now();
+
   nodemon({
     script: 'app.js',
     ext: 'js jade',
-  }).on('restart', function () {
-    setTimeout(function () {
-      livereload.changed();
+  }).on('restart', function() {
+    setTimeout(function() {
+      foso.changed();
     }, 500);
   });
 });
 
 gulp.task('default', [
-  'sass',
-  'develop',
-  'watch'
+  'develop'
 ]);
