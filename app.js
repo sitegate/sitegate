0aad19e980b6aca2dfccfad779f90462c18f6e90
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-12-29 04:01:53 +0200

    glue used.

--- app.js
+++ app.js
@@ -11,82 +11,20 @@ process.on('unhandledRejection', function(reason, p) {
 
 const config = require('./config/config');
 const Hapi = require('hapi');
-const path = require('path');
+const Glue = require('glue');
+const manifest = require('./manifest');
 require('./config/i18n');
 
 if (config.get('env') === 'development') {
   require('./config/setup-root');
 }
 
-let server = new Hapi.Server({
-  debug: {
-    log: ['error'],
-    request: ['error'],
-  },
-});
-server.connection({ port: config.get('port') });
+const options = {
+  relativeTo: __dirname,
+};
 
-server.register([
-  // registering microservices
-  { register: require('./app/plugins/client') },
-  { register: require('./app/plugins/user') },
-  { register: require('./app/plugins/session') },
-  { register: require('./app/plugins/oauth') },
-  {
-    register: require('humble-session'),
-    options: {
-      password: config.get('session.secret'),
-      cookie: 'sid-sg', // cookie name to use, usually sid-<appname>
-      isSecure: config.get('session.secure'),
-      sessionStoreName: 'session'
-    }
-  },
-  { register: require('humble-auth') },
-  { register: require('humble-flash') },
-  { register: require('hapi-auth-bearer-token') },
-  { register: require('hapi-auth-basic') },
-  { register: require('hapi-auth-form') },
-  { register: require('bell') },
-  {
-    register: require('./app/plugins/auth/auth'),
-    options: {
-      facebook: config.get('provider.facebook'),
-      google: config.get('provider.google'),
-      twitter: config.get('provider.twitter'),
-      session: config.get('session')
-    }
-  },
-  { register: require('humble-oauth2orize') },
-  { register: require('./app/plugins/oauth2') },
-  { register: require('hapi-vtree') },
-  { register: require('inert') },
-  {
-    register: require('./app/web/signin'),
-    options: {
-      homepageUrl: config.get('app.homepageUrl'),
-    },
-  },
-  {
-    register: require('./app/web/signup'),
-    options: {
-      homepageUrl: config.get('app.homepageUrl'),
-    },
-  },
-  { register: require('./app/web/reset-password') },
-  { register: require('./app/web/home') },
-  { register: require('./app/web/public') },
-  { register: require('./app/web/email') },
-  { register: require('./app/web/settings/profile') },
-  { register: require('./app/web/settings/accounts') },
-  { register: require('./app/web/settings/password') },
-  { register: require('./app/web/application') },
-  { register: require('./app/web/user-info') },
-], function(err) {
-  if (err) {
-    throw err;
-  }
+Glue.compose(manifest, options, function(err, server) {
+  if (err) throw err;
 
-  server.start(function() {
-    console.log('Server running at:', server.info.uri);
-  });
+  server.start(() => console.log('Server running at:', server.info.uri));
 });
---
+++ manifest.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const config = require('./config/config');
+
+const manifest = {
+  server: {
+    debug: {
+      log: ['error'],
+      request: ['error'],
+    },
+  },
+  connections: [
+    {
+      port: config.get('port'),
+    }
+  ],
+  registrations: [
+    // registering microservices
+    { plugin: './app/plugins/client' },
+    { plugin: './app/plugins/user' },
+    { plugin: './app/plugins/session' },
+    { plugin: './app/plugins/oauth' },
+    {
+      plugin: {
+        register: 'humble-session',
+        options: {
+          password: config.get('session.secret'),
+          cookie: 'sid-sg', // cookie name to use, usually sid-<appname>
+          isSecure: config.get('session.secure'),
+          sessionStoreName: 'session',
+        },
+      },
+    },
+    { plugin: 'humble-auth' },
+    { plugin: 'humble-flash' },
+    { plugin: 'hapi-auth-bearer-token' },
+    { plugin: 'hapi-auth-basic' },
+    { plugin: 'hapi-auth-form' },
+    { plugin: 'bell' },
+    {
+      plugin: {
+        register: './app/plugins/auth/auth',
+        options: {
+          facebook: config.get('provider.facebook'),
+          google: config.get('provider.google'),
+          twitter: config.get('provider.twitter'),
+          session: config.get('session'),
+        },
+      },
+    },
+    { plugin: 'humble-oauth2orize' },
+    { plugin: './app/plugins/oauth2' },
+    { plugin: 'hapi-vtree' },
+    { plugin: 'inert' },
+    {
+      plugin: {
+        register: './app/web/signin',
+        options: {
+          homepageUrl: config.get('app.homepageUrl'),
+        },
+      }
+    },
+    {
+      plugin: {
+        register: './app/web/signup',
+        options: {
+          homepageUrl: config.get('app.homepageUrl'),
+        },
+      },
+    },
+    { plugin: './app/web/reset-password' },
+    { plugin: './app/web/home' },
+    { plugin: './app/web/public' },
+    { plugin: './app/web/email' },
+    { plugin: './app/web/settings/profile' },
+    { plugin: './app/web/settings/accounts' },
+    { plugin: './app/web/settings/password' },
+    { plugin: './app/web/application' },
+    { plugin: './app/web/user-info' },
+  ]
+};
+
+module.exports = manifest;
